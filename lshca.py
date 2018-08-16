2c9cedc9dce6fc4e195b27a9576dbc3c1c6bd913
MichaelBr <mrbr.mail@gmail.com>
2018-08-16 23:30:43 +0300

    Added author description

--- lshca.py
+++ lshca.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python2
 
+# Description: This utility comes to provide bird's-eye view of HCAs installed.
+#              It's mainly intended for system administrators, thus defaults configured accordingly.
+# Author: Michael Braverman
+# Email: mrbr.mail@gmail.com
+# Project repo: https://gitlab.com/MrBr-gitlab/lshca/
+# License: This utility provided under GNU GPLv3 license
+
 import os
 import pickle
 import re
