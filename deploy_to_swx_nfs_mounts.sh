f7d6a2b60f93b388ea255ef2f5f46ed5f48be1a0
MichaelBr <mrbr.mail@gmail.com>
2018-11-08 22:54:03 +0200

    Fixed deployment script Fixes #44

--- deploy_to_swx_nfs_mounts.sh
+++ deploy_to_swx_nfs_mounts.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xvEe
 
 if [ $USER != root ] ; then
- echo "Run this script with sudo or under root" 
+ echo "Run this script with sudo or under root"
  exit 1
 fi
 
+tmp_dir=$(mktemp -d)
+cp lshca.py ${tmp_dir}/lshca
+chmod ugo+x ${tmp_dir}/lshca
 
 cd /hpc/local/
-cd -
 
 lab_mounts="/net/mtrlabfs01/vol/hpcvol/local/ppc64le/bin
 /net/mtrlabfs01/vol/hpcvol/local/x86_64/bin
 /net/mtrlabfs01/vol/hpcvol/local/aarch64/bin "
 
-for dest in lab_mounts; do
-  /bin/cp -f lshca.py ${dest}/lshca
+for dest in ${lab_mounts}; do
+  /bin/cp -f ${tmp_dir}/lshca ${dest}/lshca
 done
 
-scp lshca rdmz-head:/hpc/local/bin/lshca
+scp ${tmp_dir}/lshca rdmz-head:/hpc/local/bin/lshca
+
+if [ "x${tmp_dir}" != "x" ] ; then
+    rm -rf ${tmp_dir}
+fi
