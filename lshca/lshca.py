e313a110ca0e24d1a2848d1f4693da185be8059b
Michael Braverman <michaelbr@nvidia.com>
2021-01-30 12:05:04 +0200

    Moved actions from constructor to dedicated function for SYSFSDevice class

--- lshca/lshca.py
+++ lshca/lshca.py
@@ -799,13 +799,23 @@ class PCIDevice(object):
 
 class SYSFSDevice(object):
     def __init__(self, bdf, data_source, config, port=1):
-        self.bdf = bdf
-        self.port = str(port)
-        self.config = config
+        self._bdf = bdf
+        self._config = config
+        self._data_source = data_source
+        self._port = str(port)
 
-        sys_prefix = "/sys/bus/pci/devices/" + self.bdf
+        self._sys_prefix = "/sys/bus/pci/devices/" + self._bdf
 
-        vf_parent_file = data_source.read_link_if_exists(sys_prefix + "/physfn")
+    def __repr__(self):
+        delim = " "
+        return "SYS device:" + delim +\
+               self._bdf + delim + \
+               self.sriov + delim + \
+               self.vfParent + delim + \
+               self.numa
+
+    def get_data(self):
+        vf_parent_file = self._data_source.read_link_if_exists(self._sys_prefix + "/physfn")
         if vf_parent_file != "":
             self.sriov = "VF"
             self.vfParent = extract_string_by_regex(vf_parent_file, ".*\/([0-9].*)")
@@ -813,12 +823,12 @@ class SYSFSDevice(object):
             self.sriov = "PF"
             self.vfParent = "-"
 
-        self.numa = data_source.read_file_if_exists(sys_prefix + "/numa_node").rstrip()
+        self.numa = self._data_source.read_file_if_exists(self._sys_prefix + "/numa_node").rstrip()
         if not self.numa:
-            print("Warning: " + self.bdf + " has no NUMA assignment", file=sys.stderr)
+            print("Warning: " + self._bdf + " has no NUMA assignment", file=sys.stderr)
 
-        self.rdma = data_source.list_dir_if_exists(sys_prefix + "/infiniband/").rstrip()
-        net_list = data_source.list_dir_if_exists(sys_prefix + "/net/")
+        self.rdma = self._data_source.list_dir_if_exists(self._sys_prefix + "/infiniband/").rstrip()
+        net_list = self._data_source.list_dir_if_exists(self._sys_prefix + "/net/")
 
         self.net = ""
         for net in net_list.split(" "):
@@ -826,13 +836,13 @@ class SYSFSDevice(object):
             # in mlx4 dev_port has the valid value, in mlx5 - dev_id
             # this solution mimics one in ibdev2netdev
 
-            net_port_dev_id = data_source.read_file_if_exists(sys_prefix + "/net/" + net + "/dev_id")
+            net_port_dev_id = self._data_source.read_file_if_exists(self._sys_prefix + "/net/" + net + "/dev_id")
             try:
                 net_port_dev_id = int(net_port_dev_id, 16)
             except ValueError:
                 net_port_dev_id = 0
 
-            net_port_dev_port = data_source.read_file_if_exists(sys_prefix + "/net/" + net + "/dev_port")
+            net_port_dev_port = self._data_source.read_file_if_exists(self._sys_prefix + "/net/" + net + "/dev_port")
             try:
                 net_port_dev_port = int(net_port_dev_port)
             except ValueError:
@@ -845,68 +855,68 @@ class SYSFSDevice(object):
 
             net_port += 1
 
-            if str(net_port) == self.port:
+            if str(net_port) == self._port:
                 self.net = net
                 break
 
-        self.hca_type = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/hca_type").rstrip()
+        self.hca_type = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/hca_type").rstrip()
 
-        self.lnk_state = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/ports/" +
-                                                         self.port + "/state")
+        self.lnk_state = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/ports/" +
+                                                         self._port + "/state")
         self.lnk_state = extract_string_by_regex(self.lnk_state, "[0-9:]+ (.*)", "").lower()
         if self.lnk_state == "active":
             self.lnk_state = "actv"
 
         if self.lnk_state == "down":
-            self.phys_state = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                          "/ports/" + self.port + "/phys_state")
+            self.phys_state = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                          "/ports/" + self._port + "/phys_state")
             self.phys_state = extract_string_by_regex(self.phys_state, "[0-9:]+ (.*)", "").lower()
 
             if self.phys_state == "polling":
                 self.lnk_state = "poll"
 
 
-        self.link_layer = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                          "/ports/" + self.port + "/link_layer")
+        self.link_layer = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                          "/ports/" + self._port + "/link_layer")
         self.link_layer = self.link_layer.rstrip()
         if self.link_layer == "InfiniBand":
             self.link_layer = "IB"
         elif self.link_layer == "Ethernet":
             self.link_layer = "Eth"
 
-        self.fw = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/fw_ver")
+        self.fw = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/fw_ver")
         self.fw = self.fw.rstrip()
 
-        self.psid = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/board_id")
+        self.psid = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/board_id")
         self.psid = self.psid.rstrip()
 
-        self.port_rate = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/ports/" +
-                                                         self.port + "/rate")
+        self.port_rate = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/ports/" +
+                                                         self._port + "/rate")
         self.port_rate = extract_string_by_regex(self.port_rate, "([0-9]*) .*", "")
-        if self.lnk_state == "down" and self.config.show_warnings_and_errors is True:
-            self.port_rate = self.port_rate + self.config.warning_sign
+        if self.lnk_state == "down" and self._config.show_warnings_and_errors is True:
+            self.port_rate = self.port_rate + self._config.warning_sign
 
-        self.port_list = data_source.list_dir_if_exists(sys_prefix + "/infiniband/" + self.rdma + "/ports/").rstrip()
+        self.port_list = self._data_source.list_dir_if_exists(self._sys_prefix + "/infiniband/" + self.rdma + "/ports/").rstrip()
         self.port_list = self.port_list.split(" ")
 
-        self.plid = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                    "/ports/" + self.port + "/lid")
+        self.plid = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                    "/ports/" + self._port + "/lid")
         try:
             self.plid = int(self.plid, 16)
         except ValueError:
             self.plid = ""
         self.plid = str(self.plid)
 
-        self.smlid = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                     "/ports/" + self.port + "/sm_lid")
+        self.smlid = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                     "/ports/" + self._port + "/sm_lid")
         try:
             self.smlid = int(self.smlid, 16)
         except ValueError:
             self.smlid = ""
         self.smlid = str(self.smlid)
 
-        full_guid = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                    "/ports/" + self.port + "/gids/0")
+        full_guid = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                    "/ports/" + self._port + "/gids/0")
 
         self.pguid = extract_string_by_regex(full_guid, "((:[A-Fa-f0-9]{4}){4})$", "").lower()
         self.pguid = re.sub(':', '', self.pguid)
@@ -914,8 +924,8 @@ class SYSFSDevice(object):
         self.ib_net_prefix = extract_string_by_regex(full_guid, "^(([A-Fa-f0-9]{4}:){4})", "").lower()
         self.ib_net_prefix = re.sub(':', '', self.ib_net_prefix)
 
-        self.has_smi = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
-                                                       "/ports/" + self.port + "/has_smi")
+        self.has_smi = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
+                                                       "/ports/" + self._port + "/has_smi")
         self.has_smi = self.has_smi.rstrip()
         if self.link_layer != "IB" or re.match('mlx4', self.rdma):
             self.virt_hca = "N/A"
@@ -926,19 +936,19 @@ class SYSFSDevice(object):
         else:
             self.virt_hca = ""
 
-        self.sys_image_guid = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
+        self.sys_image_guid = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
                                                               "/sys_image_guid").rstrip()
 
-        self.bond_mii_status = data_source.read_file_if_exists(sys_prefix + "/net/" + self.net +
+        self.bond_mii_status = self._data_source.read_file_if_exists(self._sys_prefix + "/net/" + self.net +
                                                               "/bonding_slave/mii_status").rstrip()
-        self.bond_state = data_source.read_file_if_exists(sys_prefix + "/net/" + self.net +
+        self.bond_state = self._data_source.read_file_if_exists(self._sys_prefix + "/net/" + self.net +
                                                               "/bonding_slave/state").rstrip()
 
-        self.operstate = data_source.read_file_if_exists("/sys/class/net/" + self.net + "/operstate").rstrip()
+        self.operstate = self._data_source.read_file_if_exists("/sys/class/net/" + self.net + "/operstate").rstrip()
         self.ip_state = None
         if self.operstate == "up":
             # Implemented via shell cmd to avoid using non default libraries
-            interface_data = data_source.exec_shell_cmd(" ip address show dev %s" % self.net)
+            interface_data = self._data_source.exec_shell_cmd(" ip address show dev %s" % self.net)
             ipv4_data = find_in_list(interface_data, "inet .+")
             ipv6_data = find_in_list(interface_data, "inet6 .+")
             if ipv4_data and ipv6_data:
@@ -957,47 +967,39 @@ class SYSFSDevice(object):
         else:
             self.ip_state = "down"
 
-        tmp = data_source.list_dir_if_exists(sys_prefix + "/net/" + self.net).split(" ")
+        tmp = self._data_source.list_dir_if_exists(self._sys_prefix + "/net/" + self.net).split(" ")
         bond_master_dir = find_in_list(tmp, "upper_.*").rstrip()
         self.bond_master = extract_string_by_regex(bond_master_dir, "upper_(.*)$")
 
         if self.ip_state == "down" and ( self.lnk_state == "actv" or self.bond_state ) \
-                and self.config.show_warnings_and_errors is True:
-            self.ip_state = self.ip_state + self.config.error_sign
-        if self.ip_state == "up_noip" and self.config.show_warnings_and_errors is True:
-            self.ip_state = self.ip_state + self.config.warning_sign
+                and self._config.show_warnings_and_errors is True:
+            self.ip_state = self.ip_state + self._config.error_sign
+        if self.ip_state == "up_noip" and self._config.show_warnings_and_errors is True:
+            self.ip_state = self.ip_state + self._config.warning_sign
 
         # ========== RoCE view only related variables ==========
         self.gtclass = None
         self.tcp_ecn = None
         self.rdma_cm_tos = None
 
-        if self.config.output_view == "roce" or self.config.output_view == "all":
-            self.gtclass = data_source.read_file_if_exists(sys_prefix + "/infiniband/" + self.rdma +
+        if self._config.output_view == "roce" or self._config.output_view == "all":
+            self.gtclass = self._data_source.read_file_if_exists(self._sys_prefix + "/infiniband/" + self.rdma +
                                                            "/tc/1/traffic_class").rstrip()
-            self.tcp_ecn = data_source.read_file_if_exists("/proc/sys/net/ipv4/tcp_ecn").rstrip()
+            self.tcp_ecn = self._data_source.read_file_if_exists("/proc/sys/net/ipv4/tcp_ecn").rstrip()
 
             roce_tos_path_prefix = "/sys/kernel/config/rdma_cm/" + self.rdma
             roce_tos_path_prefix_cleanup = False
             try:
-                if data_source.list_dir_if_exists(roce_tos_path_prefix) == "":
+                if self._data_source.list_dir_if_exists(roce_tos_path_prefix) == "":
                     os.mkdir(roce_tos_path_prefix)
                     roce_tos_path_prefix_cleanup = True
-                    data_source.list_dir_if_exists(roce_tos_path_prefix) # here to record dir if recording enabled
-                self.rdma_cm_tos = data_source.read_file_if_exists(roce_tos_path_prefix +
+                    self._data_source.list_dir_if_exists(roce_tos_path_prefix) # here to record dir if recording enabled
+                self.rdma_cm_tos = self._data_source.read_file_if_exists(roce_tos_path_prefix +
                                                                    "/ports/1/default_roce_tos").rstrip()
                 if roce_tos_path_prefix_cleanup:
                     os.rmdir(roce_tos_path_prefix)
             except OSError:
                 self.rdma_cm_tos = "Failed to retrieve"
-
-    def __repr__(self):
-        delim = " "
-        return "SYS device:" + delim +\
-               self.bdf + delim + \
-               self.sriov + delim + \
-               self.vfParent + delim + \
-               self.numa
 
 
 class SaSmpQueryDevice(object):
@@ -1130,13 +1132,14 @@ class MlnxBDFDevice(object):
         self.slaveBDFDevices = []
 
         self.sysFSDevice = SYSFSDevice(self.bdf, data_source, self.config, port)
+        self.sysFSDevice.get_data()
         self.fw = self.sysFSDevice.fw
         self.hca_type = self.sysFSDevice.hca_type
         self.ib_net_prefix = self.sysFSDevice.ib_net_prefix
         self.link_layer = self.sysFSDevice.link_layer
         self.ip_state = self.sysFSDevice.ip_state
         self.pguid = self.sysFSDevice.pguid
-        self.port = self.sysFSDevice.port
+        self.port = self.sysFSDevice._port
         self.port_list = self.sysFSDevice.port_list
         self.port_rate = self.sysFSDevice.port_rate
         self.plid = self.sysFSDevice.plid
