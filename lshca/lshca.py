0b27da52dabb42c2722543b5431aa02eb023c3e0
Michael Braverman <michaelbr@nvidia.com>
2021-03-12 18:40:32 +0200

    Updated version to 3.6

--- lshca/lshca.py
+++ lshca/lshca.py
@@ -57,7 +57,7 @@ class Config(object):
         self.record_dir = "/tmp/lshca"
         self.record_tar_file = None
 
-        self.ver = "3.5"
+        self.ver = "3.6"
 
         self.mst_device_enabled = False
         self.sa_smp_query_device_enabled = False
